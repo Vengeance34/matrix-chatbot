1f4affc6362a1701b85f98f345f329969fbbf874
Vengeance34 <detective2330gmail.com>
2026-01-07 18:27:55 +0530

    Connect frontend to Render backend

--- frontend/script.js
+++ frontend/script.js
@@ -6,7 +6,7 @@ async function sendMessage() {
   addMessage("You", message, "user");
   input.value = "";
 
-  const response = await fetch("http://127.0.0.1:8000/chat", {
+  const response = await fetch("https://matrix-chatbot-jsq6.onrender.com/chat", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ message })
